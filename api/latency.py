b06603f21024ebbbea2383a5078df2f3e33ec8d1
muhyudheen <muthooos098@gmail.com>
2025-10-05 22:55:13 +0530

    Fix CORS 4th time

--- api/latency.py
+++ api/latency.py
@@ -18,6 +18,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
+    expose_headers=["*"],  # NEW: Expose version header
 )
 
 class LatencyRequest(BaseModel):
